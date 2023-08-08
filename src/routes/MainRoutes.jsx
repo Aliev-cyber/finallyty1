da07df3846a708231eababb41e6fc53540e3b9c6
ixelin <radfem54@gmail.com>
2023-08-08 17:37:19 +0600

    fast fix

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -3,12 +3,14 @@ import MainLayout from "../layouts/MainLayout";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import NotFoundPage from "../pages/NotFoundPage";
+import LikedSongs from "../pages/LikedSongs";
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/liked" element={<LikedSongs />}/>
       </Route>
       <Route path="*" element={<NotFoundPage/>}/>
     </Routes>
