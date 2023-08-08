caf5ecf78031819104b00967ef6eeed6513b76d9
ixelin <radfem54@gmail.com>
2023-08-08 17:23:54 +0600

    layout fixed

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Outlet } from "react-router-dom";
+import TheSidebar from "../pages/HomePage/components/TheSidebar";
+import TheSidebarOverlay from "../pages/HomePage/components/TheSidebarOverlay";
+import useEvent from "../pages/HomePage/hooks/useEvent";
+import TheHeader from "../pages/HomePage/components/TheHeader";
 
 const MainLayouts = () => {
+  const popoverRef = useRef();
+  const contentWrapperRef = useRef();
+  const toastRef = useRef();
+  let isScrollingEnabled = true;
+  function handleScrolling(event) {
+    if (isScrollingEnabled) return;
+
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  useEvent("wheel", handleScrolling, true, () => contentWrapperRef.current);
+
+  function showPopover(title, description, target, offset) {
+    popoverRef.current.show(title, description, target, offset);
+  }
+  function showToast(message) {
+    toastRef.current.show(message);
+  }
+  function toggleScrolling(isEnabled) {
+    isScrollingEnabled = isEnabled;
+  }
+
   return (
-    <div>
-      <Outlet />
+    <div className="flex grow overflow-auto">
+      <TheSidebar showPopover={showPopover} />
+      <TheSidebarOverlay />
+      <div className="flex-1 overflow-auto" ref={contentWrapperRef}>
+        <TheHeader />
+        <Outlet showToast={showToast} toggleScrolling={toggleScrolling} />
+      </div>
     </div>
   );
 };
