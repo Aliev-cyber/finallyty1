2006bb4ee0532a5195157b99fb27e5063774a9e6
asnblagowe <blagomukha@gmail.com>
2023-08-09 10:56:30 +0300

    fixed-liked

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import MainLayout from "../layouts/MainLayout";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import LikedSongs from "../pages/LikedSongs";
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/liked" element={<LikedSongs />} />
       </Route>
     </Routes>
   );
