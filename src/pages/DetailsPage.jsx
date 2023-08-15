6a04986cae586c8d4d5e0599b1382310a749a4cc
ixelin <radfem54@gmail.com>
2023-08-15 21:01:51 +0600

    done

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,33 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./LikedSongs.css";
 import PauseCircleIcon from "@mui/icons-material/PauseCircle";
 import PlayCircleIcon from "@mui/icons-material/PlayCircle";
 import { Box } from "@mui/material";
+import { useTracksContext } from "../contexts/TracksContext";
+import { useParams } from "react-router-dom";
 
 const DetailsPage = () => {
-  const [likedSongs, setLikedSongs] = useState([
-    {
-      image:
-        "https://resources.tidal.com/images/58cb2c48/ea88/4ffe/9847/822e0e4017e4/750x750.jpg",
-      title: "All Eyez on Me",
-      artist: "2PAC",
-      date: "1996",
-      duration: "3:30",
-      lyrics:
-        "I bet you got it twisted, you don't know who to trust So many player-hatin' - tryna sound like us Say they ready for the funk, but I don't think they knowin' Straight to the depths of Hell is where them cowards goin' Well, are you still down? Holla when you see me And let these devils be sorry for the day they finally freed me I got a caravan of - every time we ride Hittin' - up when we pass by",
-    },
-  ]);
-
-  const [isPlaying, setIsPlaying] = useState(false);
-  
+  const { getOneTrack, oneTrack } = useTracksContext();
+  const { playTrack } = useTracksContext("");
+  const [track, setTrack] = useState({});
+  const { id } = useParams();
+  useEffect(() => {
+    getOneTrack(id);
+  }, [id]);
+  useEffect(() => {
+    if (oneTrack) {
+      setTrack({ ...oneTrack });
+      playTrack("");
+    }
+  }, [oneTrack]);
+  function handleClick() {
+    playTrack(id);
+  }
   return (
     <div className="liked-songs-container">
-      {likedSongs.map((item, index) => (
-        <header key={index} className="app-header">
-          <img className="app-logo" src={item.image} alt="Spotify Logo" />
-          <h1 className="app-title">{item.title}</h1>
-        </header>
-      ))}
+      <header className="app-header">
+        <img
+          className="app-logo"
+          src={track.cover_image_url}
+          alt="Spotify Logo"
+        />
+        <h1 className="app-title">{track.title}</h1>
+      </header>
       <Box
         sx={{
           display: "flex",
@@ -35,12 +40,8 @@ const DetailsPage = () => {
           alignItems: "start",
         }}
       >
-        <div className="play-all-icon" onClick={() => setIsPlaying(!isPlaying)}>
-          {isPlaying ? (
-            <PauseCircleIcon className="icon-larger" />
-          ) : (
-            <PlayCircleIcon className="icon-larger" />
-          )}
+        <div className="play-all-icon" onClick={handleClick}>
+          <PlayCircleIcon className="icon-larger" />
         </div>
         <div
           style={{
@@ -53,22 +54,19 @@ const DetailsPage = () => {
         >
           Lyrics
         </div>
-        {likedSongs.map((item, index) => (
-          <span
-            style={{
-              marginTop: "30px",
-              textAlign: "initial",
-              marginLeft: "15px",
-              color: "white",
-              flexWrap: "wrap",
-              width: "220px",
-              fontSize: "18px",
-            }}
-            key={index}
-          >
-            {item.lyrics}
-          </span>
-        ))}
+        <span
+          style={{
+            marginTop: "30px",
+            textAlign: "initial",
+            marginLeft: "15px",
+            color: "white",
+            flexWrap: "wrap",
+            width: "220px",
+            fontSize: "18px",
+          }}
+        >
+          {track.album}
+        </span>
       </Box>
 
       {/* <main className="app-main">
@@ -76,7 +74,7 @@ const DetailsPage = () => {
           {likedSongs.map((song, index) => (
             <div
               key={index}
-              className="song-item"
+              className="song-track"
               onMouseEnter={() => handleHover(index, true)}
               onMouseLeave={() => handleHover(index, false)}
             >
